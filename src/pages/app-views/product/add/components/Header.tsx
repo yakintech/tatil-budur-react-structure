129350d26ac824c9391d4bb948361fd391caf7b8
yakintech <cagatayyildiz87@gmail.com>
2023-11-03 18:25:20 +0300

    style sample

--- src/pages/app-views/product/add/components/Header.tsx
+++ src/pages/app-views/product/add/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styled from'../style.module.css'
 
 function Header() {
   return (<>
-    <h1 className='title'>Add Product</h1>
+    <h1 className={styled.title}>Add Product</h1>
   </>
   )
 }
